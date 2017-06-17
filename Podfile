525aecec07fe57128c6ba265cb110f75c9f40aae
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-06-17 16:16:30 +0900

    プロジェクトを追加

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Spajam2017D' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Spajam2017D
+
+  target 'Spajam2017DTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Spajam2017DUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Spajam2017D/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Spajam2017D
+//
+//  Created by junpei ono on 2017/06/17.
+//  Copyright © 2017年 junpei ono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
