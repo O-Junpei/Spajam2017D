63b18a099f1cf08130f7e5a33649858e4a73915a
onojun <snorlax.chemist.and.jazz@gmail.com>
2017-06-18 02:22:54 +0900

    save

--- Spajam2017D/ChatViewController.swift
+++ Spajam2017D/ChatViewController.swift
@@ -27,52 +27,11 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
         self.viewWidth = self.view.frame.width
         self.viewHeight = self.view.frame.height
         
-        
         self.navigationController?.setNavigationBarHidden(true, animated: false)
-
         
         setDatas()
+        setView()
         
-        //Viewの背景色を設定
-        UIGraphicsBeginImageContext(self.view.frame.size)
-        UIImage(named: "background_talk")?.draw(in: self.view.bounds)
-        let image: UIImage! = UIGraphicsGetImageFromCurrentImageContext()
-        UIGraphicsEndImageContext()
-        self.view.backgroundColor = UIColor(patternImage: image)
-        
-        //戻るボタン
-        let backView:UIButton = UIButton()
-        backView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        backView.setImage(UIImage(named: "back"), for: UIControlState.normal)
-        backView.addTarget(self, action: #selector(backButtonClicked(sender:)), for:.touchUpInside)
-        
-        self.view.addSubview(backView)
-        
-        // make UIImageView instance
-        var imageView = UIImageView(frame: self.view.frame)
-        // read image
-        let image2 = UIImage(named: "table")
-        // set image to ImageView
-        imageView.image = image2
-        // set alpha value of imageView
-        imageView.alpha = 1.0
-        // set imageView to backgroundView of TableView
-        //self.tableView.backgroundView = imageView
-        chatTableView.backgroundView = imageView
-        
-        
-        //
-        //chatTableView = UITableView()
-        chatTableView.delegate = self
-        chatTableView.dataSource = self
-        chatTableView.frame = CGRect(x: 0, y: PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR, width: viewWidth, height: viewHeight-(PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR))
-        chatTableView.separatorColor = UIColor.clear
-        chatTableView.allowsSelection = false
-        chatTableView.separatorInset = UIEdgeInsets.zero
-        chatTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        chatTableView.register(RightChatTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(RightChatTableViewCell.self))
-        chatTableView.register(LeftChatTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(LeftChatTableViewCell.self))
-        self.view.addSubview(chatTableView)
     }
     
     
@@ -94,13 +53,64 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
         comment2["comment"] = "あまーーーーーーーーーーいでしょ"
         comments.append(comment2)
         
-        
         var comment3 = Dictionary<String,String>()
         comment3["user"] = "user name"
-        comment3["comment"] = "あまーーーーーーーーーーいでしょ"
+        comment3["comment"] = "あばんは、プレゼントありがょ"
         comments.append(comment3)
+        
+        var comment4 = Dictionary<String,String>()
+        comment4["user"] = "user name"
+        comment4["comment"] = "こんばんは、プレゼントありがとう"
+        comments.append(comment4)
+        
+        var comment5 = Dictionary<String,String>()
+        comment5["user"] = "onojun"
+        comment5["comment"] = "あばんは、プレゼントありがでしょ"
+        comments.append(comment5)
+        
+        var comment6 = Dictionary<String,String>()
+        comment6["user"] = "user name"
+        comment6["comment"] = "あまーーばんは、プレゼントありがいでしょ"
+        comments.append(comment6)
     }
     
+    func setView() {
+        //Viewの背景色を設定
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        UIImage(named: "background_talk")?.draw(in: self.view.bounds)
+        let backGroundImage: UIImage! = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        self.view.backgroundColor = UIColor(patternImage: backGroundImage)
+        
+        //戻るボタン
+        let backBtn:UIButton = UIButton()
+        backBtn.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        backBtn.setImage(UIImage(named: "back"), for: UIControlState.normal)
+        backBtn.addTarget(self, action: #selector(backButtonClicked(sender:)), for:.touchUpInside)
+        self.view.addSubview(backBtn)
+        
+        // make UIImageView instance
+        
+        
+        //chatTableView = UITableView()
+        chatTableView.delegate = self
+        chatTableView.dataSource = self
+        chatTableView.frame = CGRect(x: 0, y: PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR, width: viewWidth, height: viewHeight-(PARTS_HEIGHT_STATUS_BAR+PARTS_HEIGHT_NAVIGATION_BAR))
+        let tableBackGroundImage = UIImage(named: "table")
+        let tableBackImageView = UIImageView(frame: self.view.frame)
+        tableBackImageView.image = tableBackGroundImage
+        tableBackImageView.alpha = 1.0
+        chatTableView.backgroundView = tableBackImageView
+        chatTableView.separatorColor = UIColor.clear
+        chatTableView.allowsSelection = false
+        chatTableView.separatorInset = UIEdgeInsets.zero
+        chatTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        chatTableView.register(RightChatTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(RightChatTableViewCell.self))
+        chatTableView.register(LeftChatTableViewCell.self, forCellReuseIdentifier: NSStringFromClass(LeftChatTableViewCell.self))
+        self.view.addSubview(chatTableView)
+        
+    }
+        
     
     
     // MARK: - Button Action
@@ -124,7 +134,7 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
     //MARK: テーブルビューのセルの数を設定する
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         //テーブルビューのセルの数はmyItems配列の数とした
-        return 14
+        return comments.count
     }
     
     
@@ -133,9 +143,12 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //myItems配列の中身をテキストにして登録した
         
+        var comment2 = Dictionary<String,String>()
+        comment2 = comments[indexPath.row]
+        
         if indexPath.row % 2 == 0 {
             let cell:RightChatTableViewCell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(RightChatTableViewCell.self), for: indexPath) as! RightChatTableViewCell
-            cell.commentLabel.text = "右"
+            cell.commentLabel.text = comment2["comment"]
             // cellの背景を透過
             cell.backgroundColor = UIColor.clear
             // cell内のcontentViewの背景を透過
@@ -145,7 +158,7 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
             return cell
         }else{
             let cell:LeftChatTableViewCell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(LeftChatTableViewCell.self), for: indexPath) as! LeftChatTableViewCell
-            cell.commentLabel.text = "左"
+            cell.commentLabel.text = comment2["comment"]
             // cellの背景を透過
             cell.backgroundColor = UIColor.clear
             // cell内のcontentViewの背景を透過
@@ -157,6 +170,14 @@ class ChatViewController: UIViewController,UITableViewDelegate,UITableViewDataSo
         }
         
         
+    }
+    
+    
+    // MARK: - シェイクが検出されたら呼ばれる
+    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == UIEventSubtype.motionShake {
+            print("Device was shaked")
+        }
     }
 
 }
--- Spajam2017D/ViewController.swift
+++ Spajam2017D/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController {
     //width, height
     private var viewWidth:CGFloat!
     private var viewHeight:CGFloat!
+    
+    //
+    var roomNameTextField:UITextField = UITextField()
+    var userNameTextField:UITextField = UITextField()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,33 +28,64 @@ class ViewController: UIViewController {
         
         self.navigationController?.setNavigationBarHidden(true, animated: false)
         
-        
-        self.navigationController?.navigationBar.isTranslucent = false
-        
+        setView()
+    }
+    
+    //ビューに道具を登録する
+    func setView(){
         //Viewの背景色を設定
         UIGraphicsBeginImageContext(self.view.frame.size)
-        UIImage(named: "background")?.draw(in: self.view.bounds)
-        let image: UIImage! = UIGraphicsGetImageFromCurrentImageContext()
+        UIImage(named: "background_choice")?.draw(in: self.view.bounds)
+        let viewImage: UIImage! = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        self.view.backgroundColor = UIColor(patternImage: image)
+        self.view.backgroundColor = UIColor(patternImage: viewImage)
         
         //解説のラベル
         let descriptionLabel:UILabel = UILabel()
-        descriptionLabel.text = "メッセージをやり取りするには\nルーム名、自分の名前\n入力してください"
-        descriptionLabel.frame =  CGRect(x: viewWidth*0.1, y: 40, width: viewWidth*0.8, height: 5)
+        descriptionLabel.text = "メッセージをやり取りするには\nルーム名、自分の名前を\n入力してください"
+        descriptionLabel.frame =  CGRect(x: viewWidth*0.1, y: viewHeight*0.19, width: viewWidth*0.8, height: 5)
         descriptionLabel.textAlignment = NSTextAlignment.center
         descriptionLabel.numberOfLines = 0
         descriptionLabel.sizeToFit()
         self.view.addSubview(descriptionLabel)
         
+        //ルーム名
+        let roomNameLabel:UILabel = UILabel()
+        roomNameLabel.text = "ルーム名"
+        roomNameLabel.frame =  CGRect(x: viewWidth*0.1, y: viewHeight*0.38, width: viewWidth*0.8, height: viewHeight*0.05)
+        self.view.addSubview(roomNameLabel)
+        
+        //ルーム名のテキストフィールド
+        roomNameTextField.frame = CGRect(x: viewWidth*0.1, y: viewHeight*0.43, width: viewWidth*0.8, height: viewHeight*0.08)
+        roomNameTextField.text = "ルーム名を入力してください"
+        roomNameTextField.textColor = UIColor.gray
+        roomNameTextField.backgroundColor = UIColor.white
+        //titleTextField.borderStyle = UITextBorderStyle.roundedRect
+        self.view.addSubview(roomNameTextField)
+        
+        //ユーザー名
+        let userNameLabel:UILabel = UILabel()
+        userNameLabel.text = "名前"
+        userNameLabel.frame =  CGRect(x: viewWidth*0.1, y: viewHeight*0.56, width: viewWidth*0.8, height: viewHeight*0.05)
+        self.view.addSubview(userNameLabel)
+        
+        //ルーム名のテキストフィールド
+        userNameTextField.frame = CGRect(x: viewWidth*0.1, y: viewHeight*0.62, width: viewWidth*0.8, height: viewHeight*0.1)
+        userNameTextField.text = "名前"
+        userNameTextField.textColor = UIColor.gray
+        userNameTextField.backgroundColor = UIColor.white
+        self.view.addSubview(userNameTextField)
+        
         //ボタン
         let goChatVCBtn:UIButton = UIButton()
-        goChatVCBtn.frame = CGRect(x: viewWidth*0.1, y: viewHeight*0.6, width: viewWidth*0.8, height: viewWidth*0.2)
+        goChatVCBtn.frame = CGRect(x: viewWidth*0.1, y: viewHeight*0.8, width: viewWidth*0.8, height: viewWidth*0.2)
         goChatVCBtn.backgroundColor = UIColor.appRightGreen()
         goChatVCBtn.addTarget(self, action: #selector(goChatVCBtnClicked(sender:)), for:.touchUpInside)
         goChatVCBtn.setTitle("つながる", for: UIControlState.normal)
         self.view.addSubview(goChatVCBtn)
+        
     }
+    
     // MARK: - チャットボタンが押されたら呼ばれる
     func goChatVCBtnClicked(sender: UIButton){
         
@@ -59,13 +95,7 @@ class ViewController: UIViewController {
         self.navigationController?.pushViewController(chatVC, animated: true)
     }
     
-    
-    // MARK: - シェイクが検出されたら呼ばれる
-    override func motionBegan(_ motion: UIEventSubtype, with event: UIEvent?) {
-        if motion == UIEventSubtype.motionShake {
-            print("Device was shaked")
-        }
-    }
+
 
 }
 
